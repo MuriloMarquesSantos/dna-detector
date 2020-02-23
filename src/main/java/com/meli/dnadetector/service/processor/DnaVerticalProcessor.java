856252e07df7a5bc95de11c13e45c3cfa598ce4d
Murilo <murilommms@gmail.com>
2020-02-23 17:02:56 -0300

    Refactoring to remove unecessary class attribute

--- src/main/java/com/meli/dnadetector/service/processor/DnaVerticalProcessor.java
+++ src/main/java/com/meli/dnadetector/service/processor/DnaVerticalProcessor.java
@@ -6,19 +6,18 @@ import org.springframework.stereotype.Service;
 public class DnaVerticalProcessor implements DnaProcessable {
 
     public boolean processDna(String[] dna) {
-        int matrixSize = dna.length;
         char[] dnaBases = new char[dna.length];
 
         for (int i = 0; i < dna.length; i++) {
-            if (validateDnaColumns(dna, matrixSize, dnaBases, i)) {
+            if (validateDnaColumns(dna, dnaBases, i)) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean validateDnaColumns(String[] dna, int matrixSize, char[] dnaBases, int i) {
-        for (int j = 0; j < matrixSize; j++) {
+    private boolean validateDnaColumns(String[] dna, char[] dnaBases, int i) {
+        for (int j = 0; j < dna.length; j++) {
             dnaBases[j] = dna[j].charAt(i);
         }
         return validateDna(String.valueOf(dnaBases));
